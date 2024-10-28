02de1c1e2fa550cd06fc97338cc2ac916fb019b7
Ilkdjan <ilkdjanismetov@gmail.com>
2024-10-29 00:33:08 +0200

    minor changes

--- script.js
+++ script.js
@@ -1,24 +1,19 @@
 const gridContainer = document.querySelector("#gridContainer");
-function color(div)
-{
-    if(div.style.backgroundColor === "")
-    {
-        div.style.backgroundColor = "#" + Math.floor(Math.random()*16777215).toString(16)
-        div.style.opacity = 0.1;
-    }
-    else{
-        div.style.opacity = Math.min(parseFloat(div.style.opacity) + 0.1, 1);
-    }
+function color(div) {
+  if (div.style.backgroundColor === "") {
+    div.style.backgroundColor =
+      "#" + Math.floor(Math.random() * 16777215).toString(16);
+    div.style.opacity = 0.1;
+  } else {
+    div.style.opacity = Math.min(parseFloat(div.style.opacity) + 0.1, 1);
+  }
 }
 function createGrid(scale = 16) {
   for (let i = 0; i < scale * scale; i++) {
     let div = document.createElement("div");
     div.style.width = 960 / scale + "px";
     div.style.height = 960 / scale + "px";
-    div.addEventListener(
-      "mouseover",
-      () => {color(div)}
-    );
+    div.addEventListener("mouseover", () => color(div));
     gridContainer.appendChild(div);
   }
 }
